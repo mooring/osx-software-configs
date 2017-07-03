c0400585f08e28523bcd9e085c5ae6be01b372fe
mooring <mooring@live.com>
2017-07-03 16:58:39 +0800

    move to standalone repo

--- Alfred/Coder/scripts.py
+++
@@ -1,8 +0,0 @@
-from libs import CoderLib
-import sys, json
-
-temp   = '{query}'
-cmder  = json.loads(temp, encoding='utf-8')
-api    = getattr(CoderLib(), cmder['api'])
-query  = api(cmder['query'])
-sys.stdout.write(query)
